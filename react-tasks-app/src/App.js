80a314c778dafc7a18f98374287b4adf9f9eaf6c
unknown <a.petkovpetkov@gmail.com>
2022-01-19 16:20:42 +0200

    Creating react app and adding header and botton components

---
+++ react-tasks-app/src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Hello '/>
+    </div>
+  );
+}
+
+export default App;
